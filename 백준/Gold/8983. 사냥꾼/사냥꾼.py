39633f367b3ba49586045629c99ba9a186ba1a51
897754 <42639194+897754@users.noreply.github.com>
2024-07-08 13:18:42 +0900

    [Gold IV] Title: 사냥꾼, Time: 212 ms, Memory: 126072 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,7 +1,27 @@
 import sys
 
-def Distance(sr, pos):
-    return abs(sr-pos[0])+pos[1]
+# def Distance(sr, pos):
+#     return abs(sr-pos[0])+pos[1]
+
+#해당 동물이 사거리 내에 있는 sr이 있는지 반환
+def Check(srArr, a, L):
+    start = 0
+    end = len(sr)
+    range = L-a[1]
+
+    while start < end:
+        mid = (start + end) // 2
+        # 사거리 내에 sr 확인
+        if abs(srArr[mid] - a[0]) <= range:
+            return True
+        if srArr[mid] > a[0]:
+            end = mid
+        else:
+            start = mid + 1
+    
+    return False
+
+
 
 # M : 사대의 수
 # N : 동물의 수
@@ -25,35 +45,15 @@ animals = []
 for i in range(N):
     input = sys.stdin.readline().split()
     pos = (int(input[0]),int(input[1]))
-
-    # y좌표 너무 멀면 배열에 안넣음
-    # 여기는 나중에 빼고 해보자
-    if L >= pos[1]:
+    if pos[1] <= L:
         animals.append(pos)
-animals.sort(key= lambda x : x[0])
 
 # 여기서부터 로직 시작
 
 sr.sort()
 
-# j 1 4 6 9
-# i (1,4)
 count = 0
-for i in animals:
-    # 동물 기준
-    for j in sr:
-        # 사대가 너무 왼쪽에 있는 경우
-        if i[0] - L > j:
-            #print(f"Left : {i} {j}")
-            continue
-        # 사대가 너무 오른쪽에 있는 경우
-        if i[0] + L < j:
-            #print(f"Right : {i} {j}")
-            break
-        #동물 기준 한개의 사대라도 사거리 안에 있으면 count++, break.
-        if Distance(j,i) <= L:
-            count+=1
-            break
-
-
+for a in animals:
+    if Check(sr, a, L):
+        count+=1
 print(count)

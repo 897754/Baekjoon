e2bd9a27a1b7266bd0109c07757da7c66af6c0c1
897754 <42639194+897754@users.noreply.github.com>
2024-07-10 11:57:23 +0900

    [Gold II] Title: 가운데를 말해요, Time: 296 ms, Memory: 124396 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -1,29 +1,23 @@
+import heapq
 import sys
 
-def BinarySearch(arr, target):
-    left, right = 0, len(arr) - 1
-    result = -1  # 없을 경우를 대비한 기본값
+n = int(sys.stdin.readline())
 
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return mid  # target을 찾으면 해당 인덱스를 반환
-        elif arr[mid] < target:
-            result = mid  # target보다 작지만 가장 큰 값의 인덱스 저장
-            left = mid + 1
-        else:
-            right = mid - 1
+leftHeap = []
+rightHeap = []
+for i in range(n):
+    num = int(sys.stdin.readline())
 
-    return result  # target을 찾지 못했을 경우 가장 가까운 작은 값의 인덱스 반환
+    if len(leftHeap) == len(rightHeap):
+        heapq.heappush(leftHeap, -num)
+    else:
+        heapq.heappush(rightHeap, num)
 
+    if rightHeap and rightHeap[0] < -leftHeap[0]:
+        leftValue = heapq.heappop(leftHeap)
+        rightValue = heapq.heappop(rightHeap)
 
+        heapq.heappush(leftHeap, -rightValue)
+        heapq.heappush(rightHeap, -leftValue)
 
-arr = []
-# 입력
-N = int(sys.stdin.readline())
-for i in range(N):
-    X = int(sys.stdin.readline())
-    index = BinarySearch(arr, X)+1
-    arr.insert(index, X)
-    print(arr[(len(arr)-1)//2])
-
+    print(-leftHeap[0])

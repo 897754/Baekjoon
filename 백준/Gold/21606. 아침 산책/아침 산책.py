b5790faeeb48f65582a2e91cfb46642c817c2bc6
897754 <42639194+897754@users.noreply.github.com>
2024-07-13 14:48:06 +0900

    [Gold III] Title: 아침 산책, Time: 1756 ms, Memory: 93216 KB, Score: 73 point -BaekjoonHub

---
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -0,0 +1,61 @@
+import sys
+
+sys.setrecursionlimit(10**8)
+class Node:
+    def __init__(self, value) -> None:
+        self.adj = []
+        self.value = value
+    def addAdj(self, value):
+        # if value in self.adj:
+        #     return
+        self.adj.append(value)
+
+
+count = 0
+def DFS(cur:Node):
+    global count
+    visited[cur.value] = 1
+
+    #print(cur.value, end=" ")
+
+    for i in cur.adj:
+        if visited[i] != 0:
+            continue
+        #실내에 도착했을 때 카운트 올리고 리턴
+        if isInside[i]:
+            count+=1
+            continue
+        else:
+            DFS(nodes[i])
+
+            
+N = int(sys.stdin.readline())
+
+#여기 최적화 가능
+#실내 여부 입력
+input = sys.stdin.readline()
+isInside = [0]
+for i in range(N):
+    isInside.append(int(input[i]))
+
+visited = [0]*(N+1)
+# 정점 배열 초기화
+nodes = [None]
+for i in range(N):
+    nodes.append(Node(i+1))
+visited[0] = 1
+    
+
+# 간선 배열 초기화
+for i in range(N-1):
+    input = sys.stdin.readline().split()
+    nodes[int(input[0])].addAdj(int(input[1]))
+    nodes[int(input[1])].addAdj(int(input[0]))
+
+for i in range(N):
+    if isInside[i+1] == 1:
+        DFS(nodes[i+1])
+        for j in range(N):
+            visited[j+1] = 0
+
+print(count)

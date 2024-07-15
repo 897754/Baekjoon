b3d5cdcc37ac88df78f03bdc3349c731b33f1788
897754 <42639194+897754@users.noreply.github.com>
2024-07-15 20:21:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 148 ms, Memory: 115256 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -5,54 +5,58 @@ class Node:
     def __init__(self, value) -> None:
         self.adj = []
         self.value = value
-    def addAdj(self, value):
-        if value in self.adj:
+        self.visited = 0
+    def addAdj(self, node):
+        if node in self.adj:
             return
-        self.adj.append(value)
+        self.adj.append(node)
 
+def DFS(cur:Node):
+    cur.visited = 1
+    print(cur.value, end=" ")
+    for i in cur.adj:
+        if i.visited == 0:
+            DFS(i)
+    
+
+def BFS(cur:Node):
+    cur.visited = 1
+    deq.append(cur)
+    while len(deq):
+        cur = deq.popleft()
+        print(cur.value, end=" ")
+        for i in cur.adj:
+            if i.visited == 0:
+                deq.append(i)
+                i.visited = 1
+        
 input = sys.stdin.readline().split()
 N = int(input[0])
 M = int(input[1])
 V = int(input[2])
 
-visited = [0]*(N+1)
 deq = deque()
 # 정점 배열 초기화
 nodes = [None]
 for i in range(N):
     nodes.append(Node(i+1))
 
-def DFS(cur:Node):
-    visited[cur.value] = 1
-    print(cur.value, end=" ")
-    for i in cur.adj:
-        if visited[i] == 0:
-            DFS(nodes[i])
-    
-
-def BFS(cur:Node):
-    visited[cur.value] = 1
-    deq.append(cur)
-    while len(deq):
-        cur = deq.popleft()
-        print(cur.value, end=" ")
-        for i in cur.adj:
-            if visited[i] == 0:
-                deq.append(nodes[i])
-                visited[i] = 1
-        
 
 # 간선 배열 초기화
 for i in range(M):
     input = sys.stdin.readline().split()
-    nodes[int(input[0])].addAdj(int(input[1]))
-    nodes[int(input[1])].addAdj(int(input[0]))
+    a = nodes[int(input[0])]
+    b = nodes[int(input[1])]
+    a.addAdj(b)
+    b.addAdj(a)
 
+# 로직 구현
 for i in range(N):
-    nodes[i+1].adj.sort()
+    nodes[i+1].adj.sort(key=lambda x:x.value)
 
 DFS(nodes[V])
 print()
-visited = [0]*(N+1)
+for i in range(N):
+    nodes[i+1].visited = 0
+    
 BFS(nodes[V])
-# 로직 구현

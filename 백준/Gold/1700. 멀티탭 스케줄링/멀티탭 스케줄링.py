f8af72f74dbef8136b1fe479f00ea863fd644674
897754 <42639194+897754@users.noreply.github.com>
2024-09-18 00:25:31 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -1,57 +1,48 @@
 import sys
-from collections import deque 
+inf = 10**8
+input = sys.stdin.readline
 
+temp = input().split()
 
-input = sys.stdin.readline().split()
-N = int(input[0])
-K = int(input[1])
+N = int(temp[0])
+M = int(temp[1])
 
-input = sys.stdin.readline().split()
+temp = input().split()
 arr = []
-for i in range(K):
-    arr.append(int(input[i]))
+for i in temp:
+    arr.append(int(i))
 
-# qwer = deque()
-# qwer.popleft
+multitab = [-1] * N
 
-#로직 구현
-dic = {}
-for i in range(K):
-    if arr[i] in dic:
-        dic[arr[i]].append(i)
-    else:
-        dic[arr[i]] = deque()
-        dic[arr[i]].append(i)
-
-plugs = [0]*N
 count = 0
-for i in range(K):
-    count += 1
-    maxCount = 0
-    maxIndex = -1
-    q = dic[arr[i]]
-    for j in range(N):
-        if plugs[j] == 0:
-            count -= 1
-            maxIndex = j
-            break
-        if plugs[j] == arr[i]:
-            count -= 1
-            maxIndex = j
-            break
-        pQ = dic[plugs[j]]
-        if len(pQ) == 0:
-            maxCount = 1000000
-            maxIndex = j
-            continue
-        if pQ[0] > maxCount:
-            maxCount = pQ[0]
-            maxIndex = j
+# 가장 큰 루프. 사용할 기구들 돌릴때 사용됨.
+for i in range(len(arr)):
+    flag = True
+    min = (0,0)
     
-    plugs[maxIndex] = arr[i]
-
-    q.popleft()
-        
-
-
+    for j in range(len(multitab)):
+        # 이미 꽂혀있는 기구임. 넘어감
+        if arr[i] in multitab:
+            flag = False
+            break
+        # 빈 공간이 있음. 여기에 꽂음
+        if multitab[j] == -1:
+            flag = False
+            multitab[j] = arr[i]
+            break
+        # 이거 언제 재사용하는지 연산.
+        for k in range(i+1, len(arr)+1):
+            if k == len(arr):
+                min = (j, inf)
+                break
+            if(arr[k] == multitab[j]):
+                if (min[1] < k):
+                    min = (j,k)
+                break
+    if(not flag):
+        continue
+    
+    multitab[min[0]] = arr[i]
+    count+=1
+     
 print(count)
